2f1d73a336e4040c027af8fb90054d02e1e76cb2
kaharkaboodi <erfankaharkaboodi@gmail.com>
2022-05-07 03:02:42 +0430

    First commit Message

---
+++ menu.js
@@ -0,0 +1,5 @@
+let menutoggle = document.getElementById('togglemenu')
+let ulmenu = document.getElementById('itemmenu')
+menutoggle.addEventListener("click", ()=>{
+     ulmenu.classList.toggle('open')
+})
